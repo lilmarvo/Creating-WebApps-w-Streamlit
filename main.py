11512d4453b3d25f3bed0ffa46b78140f39b19a4
Adrian Phillips <fyspro@gmail.com>
2022-01-05 04:30:58 +0000

    corrected it all

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import streamlit as st
+import pandas
+
+data = {
+  'Series_1': [1,3,5,7,9],
+  'Series_2': [10,40,80,160,250]
+}
+
+df = pandas.DataFrame(data)
 
 st.title('My first streamlit App')
 st.subheader('Introduction to Steamlit: Automate it all')
 st.write('''This is my first Web App. Enjoy it!''')
-
+st.write(df)
